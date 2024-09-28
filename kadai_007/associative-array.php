75974d2e72715ee17ad4d54966c5f3f3ceb65498
ryosuke-1212 <shirataki523@gmail.com>
2024-09-28 10:15:43 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,12 @@
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+</head>
+<body>
+  <?php
+$vege_date=['name' => 'onion','price' => '200','weight' => '160'];
+
+print_r($vege_date);
+?>
+</body>
+</html>
